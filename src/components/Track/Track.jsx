406868e7c084b044693cf6c1ce4be8b6caec0b1a
talhache <adam_ibragimov_1996@mail.ru>
2023-11-27 01:01:53 +0300

    new feature: button-Pause and allocation playing track

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -2,20 +2,23 @@ import { useContext } from 'react';
 import { AudioContext } from '../../context/AudioContext';
 import styles from './Track.module.scss'
 import { IconButton } from '@mui/material';
-import { PlayArrow } from '@mui/icons-material';
+import { PlayArrow, Pause } from '@mui/icons-material';
 import secondsToMMSS from '../../utils/secondsToMMSS';
+import cn from 'classnames';
 
 const Track = (track) => {
     const {id, src, preview, title, artists, duration } = track;
 
-    const {handleToggleAudio} = useContext(AudioContext);
+    const {handleToggleAudio, currentTrack, isPlaying} = useContext(AudioContext);
+
+    const isCurrentTrack = currentTrack.id === track.id
 
     const formattedDuration = secondsToMMSS(duration);
 
     return (
-        <div className={styles.track}>
+        <div className={cn(styles.track, isCurrentTrack && styles.playing)}>
           <IconButton onClick={() => handleToggleAudio(track)}>
-            <PlayArrow/>
+           {isCurrentTrack && isPlaying ? <Pause/> : <PlayArrow/>}
           </IconButton>
           <img className={styles.preview} src={preview} alt=""/>
           <div className={styles.credits}>
